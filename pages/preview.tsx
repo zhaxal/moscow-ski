3705795cced6ec1e16087630cdd0bd993012caad
Duman Marlambekov <201605@astanait.edu.kz>
2022-12-28 21:43:50 +0600

    added preview page

---
+++ pages/preview.tsx
@@ -0,0 +1,68 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import React from "react";
+import CustomBtn from "../components/ui/CustomBtn";
+import { useRouter } from "next/router";
+import { NextPage } from "next";
+
+const Preview: NextPage = () => {
+  const route = useRouter();
+
+  return (
+    <Box
+      height="100vh"
+      sx={{ background: "linear-gradient(90deg, #0082C2 50%, #E6342D 50%)" }}
+    >
+      <Container disableGutters maxWidth="xl" sx={{ position: "relative" }}>
+        <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
+          <Box
+            component={"img"}
+            src={"/images/desktop/main-page/main-page.svg"}
+            sx={{
+              width: "100%",
+            }}
+          />
+        </Stack>
+        <Stack
+          direction={"row"}
+          sx={{
+            justifyContent: "space-around",
+            position: "absolute",
+            width: "100%",
+            bottom: 0,
+          }}
+        >
+          <Stack direction={"column"} spacing={"24px"} alignItems={"center"}>
+            <Typography variant="heading" color={"white"}>
+              ЛЫЖНЯ РОССИИ 2023
+            </Typography>
+            <CustomBtn
+              bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
+              text={"ПОСЕТИТЬ"}
+              height={"60px"}
+              width={"364px"}
+              lineHeight={"30px"}
+              fontSize={"32px"}
+              onClick={() => route.push("/ski")}
+            />
+          </Stack>
+          <Stack direction={"column"} spacing={"24px"} alignItems={"center"}>
+            <Typography variant="heading" color={"white"}>
+              МОСКОВСКИЙ ЛЫЖНЫЙ МАРАФОН
+            </Typography>
+            <CustomBtn
+              bg={"linear-gradient(90deg, #880101 0%, #EC0000 100%)"}
+              text={"ПОСЕТИТЬ"}
+              height={"60px"}
+              width={"364px"}
+              lineHeight={"30px"}
+              fontSize={"32px"}
+              onClick={() => route.push("/marathon")}
+            />
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Preview;
--- pages/ski.tsx
+++ pages/ski.tsx
@@ -5,99 +5,99 @@ import { Button, Typography } from "@mui/material";
 import CustomBtn from "../components/ui/CustomBtn";
 
 const SkiPage: NextPage = () => {
-    return (
-        <Box height="100%" sx={{ backgroundColor: "#0082C2" }}>
-            <Container disableGutters maxWidth="xl">
-                <Stack direction={"column"}>
-                    <Box sx={{ position: "relative" }}>
-                        <Box
-                            sx={{
-                                backgroundImage: "url(/images/desktop/ski-page/desc-bg.svg)",
-                                backgroundRepeat: "no-repeat",
-                                backgroundSize: "cover",
-                                minHeight: 810,
-                            }}
-                        />
-                    </Box>
+  return (
+    <Box height="100%" sx={{ backgroundColor: "#0082C2" }}>
+      <Container disableGutters maxWidth="xl">
+        <Stack direction={"column"}>
+          <Box sx={{ position: "relative" }}>
+            <Box
+              sx={{
+                backgroundImage: "url(/images/desktop/ski-page/desc-bg.svg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                minHeight: 810,
+              }}
+            />
+          </Box>
 
-                    <Stack
-                        sx={{
-                            position: "absolute",
-                            left: "650px",
-                            top: "293px",
-                            width: "991px",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography
-                            sx={{
-                                textOverflow: "ellipsis",
-                                whiteSpace: "pre-line",
-                                overflow: "hidden",
-                            }}
-                            color={"#005A87"}
-                            variant={"desc"}
-                        >
-                            «Лыжня России» - самое массовое спортивное мероприятие в России,
-                            участниками которого могут стать не только спортсмены- {"\n"}
-                            профессионалы, но и любители.
-                            {"\n"}
-                            {"\n"}С 1982 года, на протяжении вот уже 41 года, на старт по всей
-                            стране{"\n"} выходят сотни тысяч единомышленников, кто
-                            неравнодушен к лыжам, здоровому и активному отдыху.
-                        </Typography>
-                    </Stack>
-                    <Box
-                        sx={{
-                            backgroundImage: "url(/images/desktop/ski-page/center.svg)",
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "cover",
-                            minHeight: 500,
-                        }}
-                    />
+          <Stack
+            sx={{
+              position: "absolute",
+              left: "650px",
+              top: "293px",
+              width: "991px",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                textOverflow: "ellipsis",
+                whiteSpace: "pre-line",
+                overflow: "hidden",
+              }}
+              color={"#005A87"}
+              variant={"desc"}
+            >
+              «Лыжня России» - самое массовое спортивное мероприятие в России,
+              участниками которого могут стать не только спортсмены- {"\n"}
+              профессионалы, но и любители.
+              {"\n"}
+              {"\n"}С 1982 года, на протяжении вот уже 41 года, на старт по всей
+              стране{"\n"} выходят сотни тысяч единомышленников, кто
+              неравнодушен к лыжам, здоровому и активному отдыху.
+            </Typography>
+          </Stack>
+          <Box
+            sx={{
+              backgroundImage: "url(/images/desktop/ski-page/center.svg)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              minHeight: 500,
+            }}
+          />
 
-                    <Stack
-                        direction={"row"}
-                        spacing={"400px"}
-                        sx={{ px: "16px", mt: "80px" }}
-                    >
-                        <Stack direction={"column"} spacing={"48px"}>
-                            <CustomBtn
-                                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
-                                text={"ОБЩАЯ ИНФОРМАЦИЯ"}
-                                height={"48px"}
-                                lineHeight={"23px"}
-                                fontSize={"24px"}
-                            />
-                            <CustomBtn
-                                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
-                                text={"ТРЕБОВАНИЯ К УЧАСТНИКАМ"}
-                                height={"48px"}
-                                lineHeight={"15.31px"}
-                                fontSize={"16px"}
-                            />
-                        </Stack>
-                        <Stack direction={"column"} spacing={"48px"}>
-                            <CustomBtn
-                                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
-                                text={"ПРОГРАММА"}
-                                height={"48px"}
-                                lineHeight={"23px"}
-                                fontSize={"24px"}
-                            />
-                            <CustomBtn
-                                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
-                                text={"МЕСТО ПРОВЕДЕНИЯ"}
-                                height={"48px"}
-                                lineHeight={"23px"}
-                                fontSize={"24px"}
-                            />
-                        </Stack>
-                    </Stack>
-                </Stack>
-            </Container>
-        </Box>
-    );
+          <Stack
+            direction={"row"}
+            spacing={"400px"}
+            sx={{ px: "16px", mt: "80px" }}
+          >
+            <Stack direction={"column"} spacing={"48px"}>
+              <CustomBtn
+                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
+                text={"ОБЩАЯ ИНФОРМАЦИЯ"}
+                height={"48px"}
+                lineHeight={"23px"}
+                fontSize={"24px"}
+              />
+              <CustomBtn
+                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
+                text={"ТРЕБОВАНИЯ К УЧАСТНИКАМ"}
+                height={"48px"}
+                lineHeight={"15.31px"}
+                fontSize={"16px"}
+              />
+            </Stack>
+            <Stack direction={"column"} spacing={"48px"}>
+              <CustomBtn
+                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
+                text={"ПРОГРАММА"}
+                height={"48px"}
+                lineHeight={"23px"}
+                fontSize={"24px"}
+              />
+              <CustomBtn
+                bg={"linear-gradient(90deg, #015B88 0%, #009DEC 100%)"}
+                text={"МЕСТО ПРОВЕДЕНИЯ"}
+                height={"48px"}
+                lineHeight={"23px"}
+                fontSize={"24px"}
+              />
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
 
 export default SkiPage;
