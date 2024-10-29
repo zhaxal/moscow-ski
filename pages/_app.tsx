d64d80c468fce434bddb13fa154775a655227af7
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2024-10-29 11:03:55 +0900

    Refactor meta tags and add Yandex verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,22 @@ import { Box } from '@mui/material';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const metaDescription = "Московский спорт представляет большой лыжный праздник 10 и 11 февраля в Битце";
+  const metaTitle = "Лыжня России 2024 и Московский лыжный марафон";
+  const metaUrl = "https://ski.moscow.sport/";
+  const metaImage = "/images/metaIcon.jpg";
+  const yandexVerification = "5257f1e845f3792f";
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <meta name="description" content="«Московский спорт представляет большой лыжный праздник 10 и 11 февраля в Битце»" />
-        <meta property="og:title" content="«Лыжня России 2024 и Московский лыжный марафон»" />
-        <meta property="og:description" content="Московский спорт представляет большой лыжный праздник 10 и 11 февраля в Битце»" />
-        <meta property="og:url" content="https://ski.moscow.sport/" />
+        <meta name="description" content={metaDescription} />
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:url" content={metaUrl} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/images/metaIcon.jpg" />
-        <meta name="yandex-verification" content="5257f1e845f3792f" />
+        <meta property="og:image" content={metaImage} />
+        <meta name="yandex-verification" content={yandexVerification} />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -26,10 +32,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png" />
 
-        <title>Лыжня России 2024 и Московский лыжный марафон</title>
+        <title>{metaTitle}</title>
       </Head>
       <Script
-        id="id"
+        id="yandex-metrika"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -48,13 +54,13 @@ export default function App({ Component, pageProps }: AppProps) {
             `,
         }}
       />
-      <noscript><div><img src="https://mc.yandex.ru/watch/91937297" style={{ position: "absolute", left: "-9999px" }} alt="" /></div></noscript>
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/91937297" style={{ position: "absolute", left: "-9999px" }} alt="" />
+        </div>
+      </noscript>
 
-      <Box
-        sx={{ display: "none" }}
-        itemScope
-        itemType="https://schema.org/Organization"
-      >
+      <Box sx={{ display: "none" }} itemScope itemType="https://schema.org/Organization">
         <span itemProp="name">Лыжня России 2024</span>
         <div itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
           Адрес:
@@ -62,10 +68,10 @@ export default function App({ Component, pageProps }: AppProps) {
         </div>
         <span itemProp="email">ski@moscow.sport</span>
         <div itemScope itemType="http://schema.org/ImageObject">
-          <img src="/images/metaIcon.jpeg" itemProp="contentUrl" alt={"metaIcon"} />
+          <img src="/images/metaIcon.jpeg" itemProp="contentUrl" alt="metaIcon" />
         </div>
       </Box>
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
